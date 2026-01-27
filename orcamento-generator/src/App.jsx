6e33d3c89379bfc03e993f3bc9c7ed0cc2e59cb4
Gustavo <gustavogomes1108@gmail.com>
2026-01-27 13:02:48 -0300

    .

--- orcamento-generator/src/App.jsx
+++ orcamento-generator/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Documento from "./components/Documento";
 import { PDFViewer } from "@react-pdf/renderer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [showOrcamento, setShowOrcamento] = useState(false);
@@ -14,9 +14,17 @@ const App = () => {
   const [maoDeObra, setMaoDeObra] = useState([]);
   const [materiais, setMateriais] = useState([]);
 
-  const [temArea, setTemArea] = useState(false);
-  const [temMaoDeObra, setTemMaoDeObra] = useState(false);
-  const [temMateriais, setTemMateriais] = useState(false);
+  const [opcoes, setOpcoes] = useState(["Sim", "Não"]);
+
+  const [temArea, setTemArea] = useState(opcoes[1]);
+  const [temMaoDeObra, setTemMaoDeObra] = useState(opcoes[1]);
+  const [temMateriais, setTemMateriais] = useState(opcoes[1]);
+
+  useEffect(() => {
+    console.log("Tem area ", temArea);
+    console.log("Tem M.O ", temMaoDeObra);
+    console.log("Tem materiais ", temMateriais);
+  }, [temArea, temMaoDeObra, temMateriais]);
 
   return (
     <div id="main">
@@ -66,9 +74,14 @@ const App = () => {
                   type="radio"
                   name="input-area"
                   id="comArea"
+                  value={opcoes[0]}
+                  checked={temArea === opcoes[0]}
+                  onChange={(e) => {
+                    setTemArea(e.target.value);
+                  }}
                 />
                 <label className="form-check-label" htmlFor="comArea">
-                  Sim
+                  {opcoes[0]}
                 </label>
               </div>
 
@@ -78,12 +91,33 @@ const App = () => {
                   type="radio"
                   name="input-area"
                   id="semArea"
+                  value={opcoes[1]}
+                  checked={temArea === opcoes[1]}
+                  onChange={(e) => {
+                    setTemArea(e.target.value);
+                  }}
                 />
                 <label className="form-check-label" htmlFor="semArea">
-                  Não
+                  {opcoes[1]}
                 </label>
               </div>
             </div>
+
+            {temArea === opcoes[0] && (
+              <div className="mb-3">
+                <label htmlFor="area-concretada" className="form-label">
+                  Área a ser concretada
+                </label>
+                <input
+                  type="number"
+                  className="form-control"
+                  id="area-concretada"
+                  onChange={(e) => setAreaConcretada(e.target.value)}
+                  value={areaConcretada}
+                  required
+                ></input>
+              </div>
+            )}
 
             <div className="mb-3">
               <label>Tem mão de obra?</label>
@@ -94,9 +128,14 @@ const App = () => {
                   type="radio"
                   name="input-mao-de-obra"
                   id="comMaoDeObra"
+                  value={opcoes[0]}
+                  checked={temMaoDeObra === opcoes[0]}
+                  onChange={(e) => {
+                    setTemMaoDeObra(e.target.value);
+                  }}
                 />
                 <label className="form-check-label" htmlFor="comMaoDeObra">
-                  Sim
+                  {opcoes[0]}
                 </label>
               </div>
 
@@ -106,12 +145,40 @@ const App = () => {
                   type="radio"
                   name="input-mao-de-obra"
                   id="semMaoDeObra"
+                  value={opcoes[1]}
+                  checked={temMaoDeObra === opcoes[1]}
+                  onChange={(e) => {
+                    setTemMaoDeObra(e.target.value);
+                  }}
                 />
                 <label className="form-check-label" htmlFor="semMaoDeObra">
-                  Não
+                  {opcoes[1]}
                 </label>
               </div>
             </div>
+
+            {temMaoDeObra === opcoes[0] && (
+              <div className="mb-3">
+                <label htmlFor="">Itens da mão de obra</label>
+
+                <div className="input-group ">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Lançar concreto e polir"
+                    aria-label="Recipient’s username"
+                    aria-describedby="button-addon2"
+                  />
+                  <button
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    id="button-addon2"
+                  >
+                    Adicionar
+                  </button>
+                </div>
+              </div>
+            )}
 
             <div className="mb-3">
               <label>Tem materiais?</label>
@@ -122,9 +189,14 @@ const App = () => {
                   type="radio"
                   name="input-materiais"
                   id="comMateriais"
+                  value={opcoes[0]}
+                  checked={temMateriais === opcoes[0]}
+                  onChange={(e) => {
+                    setTemMateriais(e.target.value);
+                  }}
                 />
                 <label className="form-check-label" htmlFor="comMateriais">
-                  Sim
+                  {opcoes[0]}
                 </label>
               </div>
 
@@ -134,26 +206,40 @@ const App = () => {
                   type="radio"
                   name="input-materiais"
                   id="semMateriais"
+                  value={opcoes[1]}
+                  checked={temMateriais === opcoes[1]}
+                  onChange={(e) => {
+                    setTemMateriais(e.target.value);
+                  }}
                 />
                 <label className="form-check-label" htmlFor="semMateriais">
-                  Não
+                  {opcoes[1]}
                 </label>
               </div>
             </div>
 
-            <div className="mb-3">
-              <label htmlFor="area-concretada" className="form-label">
-                Área a ser concretada
-              </label>
-              <input
-                type="number"
-                className="form-control"
-                id="area-concretada"
-                onChange={(e) => setAreaConcretada(e.target.value)}
-                value={areaConcretada}
-                required
-              ></input>
-            </div>
+            {temMateriais === opcoes[0] && (
+              <div className="mb-3">
+                <label htmlFor="">Materiais a serem utilizados</label>
+
+                <div className="input-group ">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Concreto 30 MPA"
+                    aria-label="Recipient’s username"
+                    aria-describedby="button-addon2"
+                  />
+                  <button
+                    className="btn btn-outline-secondary"
+                    type="button"
+                    id="button-addon2"
+                  >
+                    Adicionar
+                  </button>
+                </div>
+              </div>
+            )}
 
             <div className="mb-3">
               <label htmlFor="preco-m2" className="form-label">
